60d3cce3681de29acb70c6bb67d9bd23b9060d07
Muhammad Imam <saitamasensei2005@gmail.com>
2022-11-18 17:15:42 +0700

    initial commit

--- src/views/AllPlayer.js
+++ src/views/AllPlayer.js
@@ -1,13 +1,67 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import axios from "axios";
+import { Table } from "react-bootstrap";
+const url = "https://jsonplaceholder.typicode.com/users";
 
-function AllPlayer(props) {
+function AllPlayer() {
+    const [user, setUser] = useState([]);
+
+
+    const getUser = async () => {
+        try {
+            let response = await axios.get(
+            "https://jsonplaceholder.typicode.com/users"
+            );
+        
+            setUser(response.data);
+
+        } catch(e){
+            console.log(e.message);
+        }
+    };
+
+    // const getUser = async () => {
+    //     const response = await fetch(url);
+    //     const newUser = await response.json();
+    //     setUser(newUser)
+    // }
+
+    useEffect(() => {
+        getUser();
+    },[]);
+
     return (
         <div>
-            <Helmet>
-             <title>All Players</title>
-            </Helmet>
-            All Players
+        <Helmet>
+            <title>All Players</title>
+        </Helmet>
+        <div className="container mt-3">
+            <div className="row">
+            <div className="col-md-12">
+                <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                            user.map((u,index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{u.email}</td>
+                                        <td>{u.phone}</td>
+                                    </tr>
+                                )
+                            })
+                    }
+                </tbody>
+                </Table>
+            </div>
+            </div>
+        </div>
         </div>
     );
 }
